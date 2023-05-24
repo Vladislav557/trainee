5560185ad4255db9d5080ac69410ac01ed7b8bd2
Vladislav Kiselev <trololo557@gmail.com>
2023-05-24 23:26:51 +0300

    Добавил сущность Продукт

---
+++ symfony/src/Entity/Product.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 6)]
+    private ?string $product_sku = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $detail_text = null;
+
+    #[ORM\ManyToOne]
+    private ?Category $category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getProductSku(): ?string
+    {
+        return $this->product_sku;
+    }
+
+    public function setProductSku(string $product_sku): self
+    {
+        $this->product_sku = $product_sku;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDetailText(): ?string
+    {
+        return $this->detail_text;
+    }
+
+    public function setDetailText(?string $detail_text): self
+    {
+        $this->detail_text = $detail_text;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
